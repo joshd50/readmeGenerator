441e8187f95f5afab049f22aa6118652ef18e1bd
JoshuaD <joshdadbin@Josh.local>
2023-03-30 13:21:15 -0700

    license badge and link rendered

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,52 +3,76 @@ const index = require('../index')
 
 
 const licenseBadges = {
-  MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
-  Apache: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
-  GPLv2: '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)',
-  GPLv3: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
-  BSD: '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+  MIT: ['[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)','https://choosealicense.com/licenses/mit/', 'MIT License'],
+  Apache: ['[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)','https://choosealicense.com/licenses/apache-2.0/', 'Apache license 2.0'],
+  GPLv2: ['[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)','https://choosealicense.com/licenses/gpl-2.0/', 'GNU General Public License v2.0'],
+  GPLv3: ['[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)', 'https://choosealicense.com/licenses/gpl-3.0/', 'GNU General Public License v3.0'],
+  BSD: ['[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)','https://choosealicense.com/licenses/0bsd/', 'BSD Zero Clause License']
 }
 
 
 
-// ['MIT', 'Apache license 2.0', 'GPLv2', 'GPLv3', 'BSD 3-Clause License', 'None']]
+// [, , 'GPLv2', 'GPLv3', 'BSD 3-Clause License', 'None']]
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 function returnBadge(license){
   const {MIT, Apache, GPLv2, GPLv3, BSD } = licenseBadges
   switch (license){
     case 'MIT' :
-      console.log(MIT);
-      break;
+      return MIT[0];
     case 'Apache license 2.0':
-      console.log(Apache);
-      break;
+      return Apache[0];
     case 'GPLv2' :
-      console.log(GPLv2);
-      break;
+      return GPLv2[0];
     case  'GPLv3':
-      console.log(GPLv3)
-      break;
+      return GPLv3[0];
     case 'BSD 3-Clause License':
-      console.log(BSD)
-      break;
+      return BSD[0];
     case 'None':
-      console.log('N/A');
-      break;
+      return '';
   }
 }
 
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badge = returnBadge(license);
+
+  return badge
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    const {MIT, Apache, GPLv2, GPLv3, BSD } = licenseBadges
+  switch (license){
+    case 'MIT' :
+      return MIT;
+    case 'Apache license 2.0':
+      return Apache;
+    case 'GPLv2' :
+      return GPLv2;
+    case  'GPLv3':
+      return GPLv3;
+    case 'BSD 3-Clause License':
+      return BSD;
+    case 'None':
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const licenselink = renderLicenseLink(license);
+  if (licenselink.length != 0) {
+    return `This application is covered by [${license[2]}](${license[1]})`
+  } else {
+    return ''
+  }
+
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -63,6 +87,7 @@ function generateMarkdown(data) {
 -${data.dsProblem}
 -${data.dsLearn}
 
+${renderLicenseBadge(data.license)}
 
 ## Table of Contents (Optional)
 
@@ -91,7 +116,6 @@ ${renderLicenseSection(data.license)}
 
 
 `;
-writeToFile('README.md',data)
 }
 
 module.exports = generateMarkdown;
